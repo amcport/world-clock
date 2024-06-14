6bab795f1e2928686416ddca0640a5eeff32dff3
amcport <143063731+amcport@users.noreply.github.com>
2024-06-14 08:10:30 +0100

    created function for static elements on page, Porto Alegre and Dublin, integrated with mode.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-alert("Hello");
+function updateTime() {
+  //Brazil
+  let brazilElement = document.querySelector("#brazil");
+  let brazilDateElement = brazilElement.querySelector(".date");
+  let brazilTimeElement = brazilElement.querySelector(".time");
+  let brazilTime = moment().tz("Brazil/East");
+
+  brazilDateElement.innerHTML = brazilTime.format("MMMM Do YYYY");
+  brazilTimeElement.innerHTML = brazilTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  //Dublin
+  let dublinElement = document.querySelector("#dublin");
+  let dublinDateElement = dublinElement.querySelector(".date");
+  let dublinTimeElement = dublinElement.querySelector(".time");
+  let dublinTime = moment().tz("Europe/Dublin");
+
+  dublinDateElement.innerHTML = dublinTime.format("MMMM Do YYYY");
+  dublinTimeElement.innerHTML = dublinTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
